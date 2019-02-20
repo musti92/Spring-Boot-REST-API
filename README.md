09d13a404a471c43db62f25e16cd920875c7c9ec
musti4242 <43019400+musti4242@users.noreply.github.com>
2019-02-20 07:29:27 +0300

    Spring-Boot REST API

---
+++ README.md
@@ -0,0 +1,3 @@
+# Spring-Boot-REST-API
+
+Spring Boot  , Spring Data JPA and MYSQL içeriğine sahip basit bir REST API örneğidir.
---
+++ src/main/java/com/example/service/CricketerServiceImpl.java
@@ -0,0 +1,31 @@
+package com.example.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.stereotype.Service;
+
+import com.example.model.Cricketer;
+import com.example.repository.CricketerRepository;
+
+@Service
+public class CricketerServiceImpl implements CricketerService{
+
+	@Autowired
+	CricketerRepository cricketerRepository;
+	
+	@Override
+	public void save(Cricketer cricketer) {
+		cricketerRepository.save(cricketer);
+	}
+
+	public Cricketer findById(Long id){
+		return cricketerRepository.findOne(id);
+	}
+
+	@Cacheable("cricketers")
+	public List<Cricketer> getAllPlayers(){
+		return cricketerRepository.findAll();
+	}
+}
